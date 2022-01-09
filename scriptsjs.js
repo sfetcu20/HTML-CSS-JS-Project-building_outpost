7fefed1e095ca70ef7936b5cad34a9cb0de6f431
Knv88 <80967324+Knv88@users.noreply.github.com>
2022-01-09 17:45:12 +0300

    ia deaici

--- scriptsjs.js
+++ scriptsjs.js
@@ -1,11 +1,17 @@
-
+function preload()
+{
+  // Spiderman Mask Filter asset
+  imgstar = loadImage("./stars-png-mpd-fl-stars-image-638-2438.png");
+  
+}
 window.onload=()=>{
  
 var canvas = document.getElementById('can');
 var context=canvas.getContext('2d');
 var video = document.getElementById('video');
-var width = 350;
-var height = 200;
+
+var width = 400;
+var height = 150;
 
   navigator.mediaDevices.getUserMedia({
     video: true,
@@ -15,17 +21,44 @@ var height = 200;
 video.play();
 });
 function computeClearFrame(){
-    context.drawImage(video, -30, -30, width, height);
+    
+    context.drawImage(video, -30, -10, width, height);
+    
+  
 }
+
 function render(){
     computeClearFrame();
+ 
     setTimeout(() => {
         render();
     }, 0);
 }
+var c=document.getElementById('cafea');
+c.addEventListener("click",()=>
+{
+    i=document.getElementById("stars");
+    if(i.hasAttribute('hidden')) i.removeAttribute('hidden');
+    else i.setAttribute('hidden','true');
+})
+img = document.getElementById('rama');
+img.addEventListener("click",()=>{
+    context.filter="none";
+    render();
+});
 
-img = document.getElementById('image');
-img.addEventListener("click",render);
+}
 
+
+
+
+
+
+function addfilter(id){
+    console.log(id);
+    var canvas = document.getElementById('can');
+    var context=canvas.getContext('2d');
+    context.filter="url(#fl_" + id +")";
+    render();
 
 }
